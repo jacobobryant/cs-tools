fcd7df57bd2d524d5c59bb86238ad54ae5fbde46
Jacob O'Bryant <tooke@gmx.com>
2013-04-21 21:32:31 -0700

    started making add_docs.py more object-oriented

--- add_docs.py
+++ add_docs.py
@@ -5,59 +5,75 @@ import sys
 
 KEY_LENGTH = len("Description:")  # That's the longest key.
 
-def hline(func, key, value=''):
-    keyString = "{0: <{1}}".format(key + ":", KEY_LENGTH)
-    return "{0}// {1} {2}\n".format(func['indent'], keyString, value)
+class Header:
+    def __init__(self, func, name, width, position):
+        self.func = func
+        self.name = name
+        self.width = width
+        self.position = position
+        self.calls = []
 
-def box_top(func, width):
-    TAB_WIDTH = 4
-    numTabs = func['indent'].count('\t')
-    width = width - numTabs * (TAB_WIDTH - 1)
-    return "{0:-<{1}}\n".format(func['indent'] + '//', width)
+        TAB_WIDTH = 4
+        numTabs = func['indent'].count('\t')
+        width -= numTabs * (TAB_WIDTH - 1)
+        self._box_top = "{0:-<{1}}\n".format(func['indent'] + '//', width)
 
-def params(func):
-    args = re.sub(r'\s+', ' ', func['args'])
-    args = [arg.strip() for arg in args.split(',')]
-    width = max([len(arg) for arg in args]) + len(":")
-    argString = lambda arg: "{0: <{1}}".format(arg + ":", width)
-    line = lambda arg: func['indent'] + "// " + ' ' * (KEY_LENGTH + 1) + \
-            argString(arg) + "\n"
+    def add_calls(self, text, names):
+        pass
 
-    ret = ""
-    if args[0]:
-        ret += hline(func, "Parameters", argString(args[0]))
-        for arg in args[1:]:
-            ret += line(arg)
-    return ret
+    def text(self):
+        h = ""
+        h += self._box_top()
+        h += self._signature()
+        h += self._hline("Description")
+        h += self._hline("Programmers", name)
+        h += self._hline("Version", "1.0")
+        h += self._hline("Input")
+        h += self._hline("Output")
+        h += self._hline("Calls")
+        h += self._hline("Called by")
+        h += self._params()
+        if self.func['type'] != 'void':
+            h += self._hline("Returns")
+        h += self._hline("History")
+        h += self._box_top()
+        return h
 
-def signature(func):
-    args = re.sub(r'\s+', ' ', func['args'])
-    signature = "{0}({1})".format(func['name'], args)
-    if func['class']:
-        return hline(func, "Class", func['class']) + \
-                hline(func, "Method", signature)
-    else:
-        return hline(func, "Function", signature)
+    def _hline(self, key, value=''):
+        keyString = "{0: <{1}}".format(key + ":", KEY_LENGTH)
+        return "{0}// {1} {2}\n".format(self.func['indent'], keyString, value)
 
-def build_header(func, name, width):
-    h = ""
-    h += box_top(func, width)
-    h += signature(func)
-    h += hline(func, "Description")
-    h += hline(func, "Programmers", name)
-    h += hline(func, "Version", "1.0")
-    h += hline(func, "Input")
-    h += hline(func, "Output")
-    h += hline(func, "Calls")
-    h += hline(func, "Called by")
-    h += params(func)
-    if func['type'] != 'void':
-        h += hline(func, "Returns")
-    h += hline(func, "History")
-    h += box_top(func, width)
-    return h
+    def _box_top(self):
+        TAB_WIDTH = 4
+        numTabs = self.func['indent'].count('\t')
+        width = self.width - numTabs * (TAB_WIDTH - 1)
+        return "{0:-<{1}}\n".format(self.func['indent'] + '//', width)
 
-def build_headers(infile):
+    def _params(self):
+        args = re.sub(r'\s+', ' ', func['args'])
+        args = [arg.strip() for arg in args.split(',')]
+        width = max([len(arg) for arg in args]) + len(":")
+        argString = lambda arg: "{0: <{1}}".format(arg + ":", width)
+        line = lambda arg: func['indent'] + "// " + ' ' * (KEY_LENGTH + 1) + \
+                argString(arg) + "\n"
+
+        ret = ""
+        if args[0]:
+            ret += hline(func, "Parameters", argString(args[0]))
+            for arg in args[1:]:
+                ret += line(arg)
+        return ret
+
+    def _signature(self):
+        args = re.sub(r'\s+', ' ', self.func['args'])
+        signature = "{0}({1})".format(self.func['name'], args)
+        if self.func['class']:
+            return self._hline("Class", self.func['class']) + \
+                    self._hline("Method", signature)
+        else:
+            return self._hline("Function", signature)
+
+def build_headers(text):
     headers = []
     pattern = re.compile(r"""
             ^(?P<indent>\s*)
@@ -67,56 +83,62 @@ def build_headers(infile):
             \(
             (?P<args>[^)]*)
             \)(?!;)
-            """, re.VERBOSE|re.MULTILINE|re.DOTALL)
-    for match in pattern.finditer(infile.read()):
+            """, re.VERBOSE|re.MULTILINE)
+    for match in pattern.finditer(text):
         func = match.groupdict()
         # Type can only be ommited for constructors/destructors
-        if not func['type'] and (func['class'] == None or
+        if not func['type'] and (not func['class'] or
                 func['class'] != func['name']):
             continue
-        headers.append({'position': match.start(),
-            'text': build_header(match.groupdict(), args.name, args.width)})
+        headers.append(Header(func, args.name, args.width, match.start()))
+#        headers.append({'position': match.start(), 'text':
+#            build_header(match.groupdict(), args.name, args.width)})
     return headers
 
-def insert_headers(infile, headers):
-    charsRead = 0
+def insert_headers(text, headers):
     output = ""
+    position = 0
     for header in headers:
-        amount = header['position'] - charsRead
-        charsRead += amount
-        output += infile.read(amount)
-        output += header['text']
-    output += infile.read()
+        output += text[position:header.position]
+        output += header.text()
+        position = header.position
+    output += text[position:]
     return output
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-            description="Add function headers to c++ source files. Writes"
-            + " to stdout unless the -i option is included.")
-    parser.add_argument("files", nargs="+", help="the .cpp files")
-    parser.add_argument("-n", "--name", default='Jacob O\'Bryant',
-            help="Your name")
-    parser.add_argument("-w", "--width", type=int, default=74,
-            help="The maximum line width")
-    parser.add_argument("-i", "--in-place", action="store_true",
-            help="modify file in place")
-    args = parser.parse_args()
+parser = argparse.ArgumentParser(
+        description="Add function headers to c++ source files. Writes"
+        + " to stdout unless the -i option is included.")
+parser.add_argument("files", nargs="+", help="the .cpp files")
+parser.add_argument("-n", "--name", default='Jacob O\'Bryant',
+        help="Your name")
+parser.add_argument("-w", "--width", type=int, default=74,
+        help="The maximum line width")
+parser.add_argument("-i", "--in-place", action="store_true",
+        help="modify file in place")
+args = parser.parse_args()
 
-    for filename in args.files:
-        try:
-            infile = open(filename, "r")
-        except IOError:
-            sys.stderr.write("Couldn't open " + filename + " for reading.\n")
-            continue
-        headers = build_headers(infile)
-        infile.seek(0)
-        output = insert_headers(infile, headers)
-        infile.close()
-        try:
-            outfile = open(filename, 'w') if args.in_place else sys.stdout
-        except IOError:
-            sys.stderr.write("Couldn't open " + filename + " for writing.\n")
-            continue
-        outfile.write(output)
-        if outfile is not sys.stdout:
-            outfile.close()
+for filename in args.files:
+    try:
+        infile = open(filename, "r")
+    except IOError:
+        sys.stderr.write("Couldn't open " + filename +
+                " for reading.\n")
+        continue
+    text = infile.read()
+    infile.close()
+    headers = build_headers(text)
+    names = set([h.func['name'] for h in headers])
+    '''
+    for h in headers:
+        h.add_calls(text, names)
+    '''
+    output = insert_headers(text, headers)
+    try:
+        outfile = open(filename, 'w') if args.in_place else sys.stdout
+    except IOError:
+        sys.stderr.write("Couldn't open " + filename +
+                " for writing.\n")
+        continue
+    outfile.write(output)
+    if outfile is not sys.stdout:
+        outfile.close()
