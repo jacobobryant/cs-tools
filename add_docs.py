1966a9473d3d00e768fdafcb0c2a503afdd532ca
Jacob O'Bryant <tooke@gmx.com>
2013-04-23 13:10:21 -0700

    Separated Header class into BaseHeader and other classes

--- add_docs.py
+++ add_docs.py
@@ -3,17 +3,96 @@ import re
 import argparse
 import sys
 
-KEY_LENGTH = len("Description:")  # That's the longest key.
+class BaseHeader:
+    indent = ""
+    position = 0
+    contents = []
+    key_length = 0
 
-class Header:
+    def __str__(self):
+        self.key_length = max([len(k) for k, v in self.contents]) \
+                + len(": ")
+        ret = self.box_top()
+        for k, v in self.contents:
+            if isinstance(v, str):
+                ret += self.hline(k, v)
+            else:
+                ret += self.hlines(k, v)
+        ret += self.box_top()
+        return ret
+
+    def hline(self, key, value=''):
+        if key:
+            key += ":"
+        keyString = "{0: <{1}}".format(key, self.key_length)
+        return "{0}// {1}{2}\n".format(self.indent, keyString, value)
+
+    def hlines(self, key, values):
+        ret = ''
+        if values:
+            ret += self.hline(key, values[0])
+            ret += ''.join([self.hline('', value) for value in values[1:]])
+        return ret
+
+    def box_top(self):
+        numTabs = self.indent.count('\t')
+        width = args.width - numTabs * (args.tab_width - 1)
+        return "{0:-<{1}}\n".format(self.indent + '//', width)
+
+    @staticmethod
+    def equalize(keys):
+        width = max([len(key) for key in keys]) + len(": ")
+        keyString = lambda key: "{0: <{1}}".format(key + ":", width)
+        return [keyString(key) for key in keys]
+
+    @staticmethod
+    def merge(text, headers):
+        # This assumes that the headers are already sorted by position.
+        output = ""
+        position = 0
+        for header in headers:
+            output += text[position:header.position]
+            output += str(header)
+            position = header.position
+        output += text[position:]
+        return output
+
+class FunctionHeader(BaseHeader):
     def __init__(self, func, position):
-        self.func = func
+        self.indent = func['indent']
+        self.type = func['type']
+        self.class_ = func['class']
+        self.name = func['name']
+        self.args = re.sub(r'\s+', ' ', func['args'])
         self.position = position
         self.calls = []
         self.called_by = []
+        self.signature = "{0}({1})".format(self.name, self.args)
+        self.definition = self.type + ' ' + self.signature
+
+    @classmethod
+    def parse(cls, text):
+        headers = []
+        pattern = re.compile(r"""
+                ^(?P<indent>\s*)
+                (?P<type>((const\ )?[\w&*]+\ )?)
+                ((?P<class>\w+)::)?
+                (?P<name>(\w+|operator\S\S?))
+                \(
+                (?P<args>[^)]*)
+                \)(?!;)
+                """, re.VERBOSE|re.MULTILINE)
+        for match in pattern.finditer(text):
+            func = match.groupdict()
+            # Type can only be ommited for constructors/destructors
+            if not func['type'] and (not func['class'] or
+                    func['class'] != func['name']):
+                continue
+            headers.append(cls(func, match.start()))
+        return headers
 
     def set_calls(self, text, names):
-        block = self._get_block(text)
+        block = self.get_block(text)
         pattern = re.compile(r'[^:](\w+)\(')
         matches = set(pattern.findall(block))
         self.calls = [name for name in matches if name in names]
@@ -21,10 +100,10 @@ class Header:
     def set_called_by(self, headers):
         for called in self.calls:
             for header in headers:
-                if header.func['name'] == called:
-                    header.called_by.append(self.func['name'])
+                if header.name == called:
+                    header.called_by.append(self.name)
 
-    def _get_block(self, text):
+    def get_block(self, text):
         start = text.find("{", self.position)
         level = 0
         for i in range(start, len(text)):
@@ -37,89 +116,47 @@ class Header:
                 break
         return text[start:end]
 
-    def text(self):
-        h = ""
-        h += self._box_top()
-        h += self._signature()
-        h += self._hline("Description")
-        h += self._hline("Programmers", args.name)
-        h += self._hline("Version", "1.0")
-        h += self._hline("Input")
-        h += self._hline("Output")
+    def __str__(self):
+        self.contents = self.title() + \
+                [["Description", ""],
+                 ["Programmers", args.name],
+                 ["Version", args.version],
+                 ["Environment", args.environment],
+                 ["Input", ""],
+                 ["Output", ""]]
         if self.calls:
-            h += self._hline("Calls", ", ".join(self.calls))
+            self.contents.append(["Calls", ", ".join(self.calls)])
         if self.called_by:
-            h += self._hline("Called by", ", ".join(self.called_by))
-        h += self._params()
-        if self.func['type'] != 'void':
-            h += self._hline("Returns")
-        h += self._hline("History")
-        h += self._box_top()
-        return h
+            self.contents.append(["Called by", ", ".join(self.called_by)])
+        if self.args:
+            self.contents.append(["Parameters", self.params()])
+        if self.type != 'void':
+            self.contents.append(["Returns", ""])
+        self.contents.append(["History", ""])
+        return super().__str__()
 
-    def _hline(self, key, value=''):
-        keyString = "{0: <{1}}".format(key + ":", KEY_LENGTH)
-        return "{0}// {1} {2}\n".format(self.func['indent'], keyString, value)
+    def params(self):
+        args = [arg.strip() for arg in self.args.split(',')]
+        return self.equalize(args)
 
-    def _box_top(self):
-        TAB_WIDTH = 4
-        numTabs = self.func['indent'].count('\t')
-        width = args.width - numTabs * (TAB_WIDTH - 1)
-        return "{0:-<{1}}\n".format(self.func['indent'] + '//', width)
-
-    def _params(self):
-        args = re.sub(r'\s+', ' ', self.func['args'])
-        args = [arg.strip() for arg in args.split(',')]
-        width = max([len(arg) for arg in args]) + len(":")
-        argString = lambda arg: "{0: <{1}}".format(arg + ":", width)
-        line = lambda arg: self.func['indent'] + "// " + ' ' * (KEY_LENGTH + 1) + \
-                argString(arg) + "\n"
-
-        ret = ""
-        if args[0]:
-            ret += self._hline("Parameters", argString(args[0]))
-            for arg in args[1:]:
-                ret += line(arg)
-        return ret
-
-    def _signature(self):
-        args = re.sub(r'\s+', ' ', self.func['args'])
-        signature = "{0}({1})".format(self.func['name'], args)
-        if self.func['class']:
-            return self._hline("Class", self.func['class']) + \
-                    self._hline("Method", signature)
+    def title(self):
+        if self.class_:
+            return [["Class", self.class_],
+                    ["Method", self.signature]]
         else:
-            return self._hline("Function", signature)
+            return [["Function", self.signature]]
 
-def build_headers(text):
-    headers = []
-    pattern = re.compile(r"""
-            ^(?P<indent>\s*)
-            (?P<type>((const\ )?[\w&*]+\ )?)
-            ((?P<class>\w+)::)?
-            (?P<name>(\w+|operator\S\S?))
-            \(
-            (?P<args>[^)]*)
-            \)(?!;)
-            """, re.VERBOSE|re.MULTILINE)
-    for match in pattern.finditer(text):
-        func = match.groupdict()
-        # Type can only be ommited for constructors/destructors
-        if not func['type'] and (not func['class'] or
-                func['class'] != func['name']):
-            continue
-        headers.append(Header(func, match.start()))
-    return headers
+class CPPFileHeader(BaseHeader):
+    def __init__(self, filename, headers):
+        functions = [header.definition for header in headers]
+        self.contents = [["File", filename],
+                         ["Functions", functions]]
 
-def insert_headers(text, headers):
-    output = ""
-    position = 0
-    for header in headers:
-        output += text[position:header.position]
-        output += header.text()
-        position = header.position
-    output += text[position:]
-    return output
+class HFileHeader(BaseHeader):
+    def __init__(self, filename, text):
+        classes = re.findall(r'\s*class\s+(\w+)', text)
+        self.contents = [["File", filename],
+                         ["Classes", classes]]
 
 parser = argparse.ArgumentParser(
         description="Add function headers to c++ source files. Writes"
@@ -131,6 +168,12 @@ parser.add_argument("-w", "--width", type=int, default=74,
         help="The maximum line width")
 parser.add_argument("-i", "--in-place", action="store_true",
         help="modify file in place")
+parser.add_argument("-e", "--environment", default="Intel Pentium PC, " +
+        "Arch Linux, gcc 4.7", help="The maximum line width")
+parser.add_argument("-t", "--tab-width", type=int, default=4,
+        help="The width of tabs")
+parser.add_argument("-v", "--version", default="1.0",
+        help="The starting version")
 args = parser.parse_args()
 
 for filename in args.files:
@@ -142,12 +185,13 @@ for filename in args.files:
         continue
     text = infile.read()
     infile.close()
-    headers = build_headers(text)
-    names = set([h.func['name'] for h in headers])
+    headers = FunctionHeader.parse(text)
+    fileHeader = CPPFileHeader(filename, headers)
+    names = set([h.name for h in headers])
     for h in headers:
         h.set_calls(text, names)
         h.set_called_by(headers)
-    output = insert_headers(text, headers)
+    output = BaseHeader.merge(text, [fileHeader] + headers)
     try:
         outfile = open(filename, 'w') if args.in_place else sys.stdout
     except IOError:
